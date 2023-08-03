efd6a11a5ed67d3d92b7639cd9989364e7a96138
Mateusz Such <suchmateusz94@gmail.com>
2023-08-03 18:06:52 +0200

    Migracja

---
+++ WebAppMVC.Infrastructure/Migrations/20230803160545_InitialCreate2.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppMVC.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemId",
+                table: "Types",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemCategoryId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ItemCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemCategoryId",
+                table: "Items",
+                column: "ItemCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemCategory_ItemCategoryId",
+                table: "Items",
+                column: "ItemCategoryId",
+                principalTable: "ItemCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemCategory_ItemCategoryId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "ItemCategory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_ItemCategoryId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Types");
+
+            migrationBuilder.DropColumn(
+                name: "ItemCategoryId",
+                table: "Items");
+        }
+    }
+}
